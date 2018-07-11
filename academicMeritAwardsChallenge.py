116e69ffd8f67b5e951732e7a517733d4e6d24a5
EthanCornish <19ecornish@mlmc.vic.edu.au>
2018-07-11 17:38:49 +0930

    Working on the Academic and Merit Awards Holiday Challenge. Development finished. Testing to be done.

--- academicMeritAwardsChallenge.py
+++ academicMeritAwardsChallenge.py
@@ -1,9 +1,18 @@
+# exit code 1 is for incorrect input for file name and directory
+# exit code 2 is for incorrect file format for merit input file
+
 # CSV library used when reading files to automatically strip the \n from each line and split at each comma into a list
 import csv
 
-# Defining listAcademic1, reading file (fileName will be input from user)
+# Defining listAcademic1, reading file
 listAcademic1 = []
-aImportFile = open('/Users/19ecornish/Downloads/noe-comp.xlsx - academic.csv', 'r')
+fileName = input('Directory and Name of location for Academic Input .csv file. '
+                 'e.g. /Users/19ecornish/Downloads/academic.csv\n:')
+try:
+    aImportFile = open(fileName, 'r')
+except FileNotFoundError:
+    print('The input file could not be found.\nExiting the program.')
+    exit(1)
 # Using csv.reader to strip and split each line.
 for line in csv.reader(aImportFile):
     listAcademic1.append(line)
@@ -107,6 +116,19 @@ while count < total:
         number += 1
     count += 1
 
+# Resetting number, cycle and total for loop
+number = 0
+cycle = 0
+total = len(listAcademic2)
+# Loop to remove any records that have no academic awards
+while number < total:
+    # If the subjects sublist is empty
+    if listAcademic2[number-cycle][4] == []:
+        # Remove the record
+        poppedList.append(listAcademic2.pop(number-cycle))
+        cycle += 1
+    number += 1
+
 # Taking listAcademic2 and converting it into listAcademic3
 
 # Defining listAcademic 3 and resetting number
@@ -130,25 +152,61 @@ for a in listAcademic2:
 
 # Defining listMerit1, reading file (fileName will be input from user)
 listMerit1 = []
-mImportFile = open('/Users/19ecornish/Downloads/noe-comp.xlsx - merit copy.csv', 'r')
+fileName = input('\nDirectory and Name of location for Merit Input .csv file. '
+                 'e.g. /Users/19ecornish/Downloads/merit.csv\n:')
+try:
+    mImportFile = open(fileName, 'r')
+except FileNotFoundError:
+    print('The input file could not be found.\nExiting the program.')
+    exit(1)
 # Using csv.reader to strip and split each line.
 for line in csv.reader(mImportFile):
    listMerit1.append(line)
 mImportFile.close()
 
-# Remove the header record from the array
-poppedList.append(listMerit1.pop(0))
-# Rearranging the order of the list in the same way listAcademic1 was done
-for record in listMerit1:
-    tempList = record
-    record = []
-    record.append(tempList[0])
-    record.append(tempList[2])
-    record.append(tempList[1])
-    record.append(tempList[3])
-    record.append(tempList[4])
-    record.append(tempList[5])
+# If the merit input is in the form: UID, Surname, Preferred, Year Level, Class Description with another column for
+#   score then this block should run. It was in this form in the sample input file however there was no column for score
+#   so I added one with both 'merit' and 'none' as the data for testing.
+if len(listMerit1[0]) == 6:
+    # Remove the header record from the array
+    poppedList.append(listMerit1.pop(0))
+    # Rearranging the order of the list in the same way listAcademic1 was done
+    for record in listMerit1:
+        tempList = record
+        record = []
+        record.append(tempList[0])
+        record.append(tempList[2])
+        record.append(tempList[1])
+        record.append(tempList[3])
+        record.append(tempList[4])
+        record.append(tempList[5])
 
+# If the merit input file is in the form: UID, Surname, Preferred, Gender, Year Level, Class Description, Class Teacher,
+#    Score as it is on the instructions then this block should run and output listMerit1 as shown in textfile
+elif len(listMerit1[0]) == 8:
+    # Remove the header record from the array
+    poppedList.append(listMerit1.pop(0))
+    # Rearranging the order of the list in the same way listAcademic1 was done
+    for record in listMerit1:
+        # Popping all of the unimportant information
+        poppedList.append(record.pop(3))
+        poppedList.append(record.pop(5))
+
+        # Rearranging the order of the list
+        # Make a copy of the sublist, wipe original sublist then add things back in correct order
+        tempList = record
+        record = []
+        record.append(tempList[0])
+        record.append(tempList[2])
+        record.append(tempList[1])
+        record.append(tempList[3])
+        record.append(tempList[4])
+        record.append(tempList[5])
+        listAcademic1[number] = record
+        number += 1
+else:
+    print('Merit Input File Format was not Recignised.')
+    exit(2)
 
 # Sorts listMerit1 by student number
 listMerit1.sort()
@@ -161,7 +219,8 @@ cycles = 0
 # Total for the while loop. Set now as the len of the list will change due to popping
 total = len(listMerit1)
 while number < total:
-    if listMerit1[number-cycles][5] == 'none':
+    if listMerit1[number-cycles][5] == 'none' or listMerit1[number-cycles][5] == 'None' or\
+            listMerit1[number-cycles][5] == 'NONE':
         poppedList.append(listMerit1.pop(number-cycles))
         cycles += 1
     number += 1
@@ -204,44 +263,112 @@ for d in listMerit2:
 
 # Creating listOutput as shown in textfile
 
-# Defining list and resetting number
+# Defining list
 listOutput = []
-listOutput.append(['UID', 'Preferred', 'Surname', 'Year', 'Academic', 'Count A', '',
-                   'UID', 'Preferred', 'Surname', 'Year', 'Merit', 'Count M'])
-number = 0
 # Space is used as a placeholder for the empty column in the output table
 space = ''
 # Loop goes through each record in ListAcademic 3 and if there is also a
 #       record for merit for the same student then it is added as one record
 #       otherwise the record is left as just the academic with place holders
+
+# Goes through each record in academic list
 for i in listAcademic3:
+    # found records if there is a match in the UIDs for academic and merit lists
     found = False
+    # Goes through each record for merit list resetting cycle each time
+    cycle = 0
     for item in listMerit3:
+        # Match found
         if item[0] == i[0]:
             found = True
             break
+        cycle += 1
     if found:
+        # Add a record to listOutput formatted according to instructions with both academic and merit
         listOutput.append(
-            [i[0], i[1], i[2], i[3], i[4], i[5], space, listMerit3[number][0], listMerit3[number][1],
-                listMerit3[number][2], listMerit3[number][3], listMerit3[number][4], listMerit3[number][5]])
+            [i[0], i[1], i[2], i[3], i[4], i[5], space, listMerit3[cycle][0], listMerit3[cycle][1],
+                listMerit3[cycle][2], listMerit3[cycle][3], listMerit3[cycle][4], listMerit3[cycle][5]])
     elif not found:
+        # Add a record to listOutput only with values for academic awards, merit values are left blank
         listOutput.append([i[0], i[1], i[2], i[3], i[4], i[5], space, space, space, space, space, space, space])
-    number += 1
-    print('\n')
 
-print('\nlistOutput V1')
-for record in listOutput:
-    print(record)
-
+# Loop checks if there are any records in the merit list that haven't
+#   already been added to output list. If there are it adds them
 for i in listMerit3:
+    # records if a match was found
     found = False
+    # Goes through each record already in output list
     for item in listOutput:
         if i[0] == item[0]:
+            # match found, exits sub for loop. Nothing needs to be added
             found = True
             break
     if not found:
+        # If the match was not found then add the merit record to output list using placeholders for academic values
         listOutput.append([space, space, space, space, space, space, space, i[0], i[1], i[2], i[3], i[4], i[5]])
 
-print('\nlistOutput V2')
+
+fileName = input('\nName and Directory to place output file. '
+                       'e.g. /Users/19ecornish/Desktop/AcademicMeritOutput.csv\n:')
+
+try:
+    outputFile = open(fileName, 'w')
+except FileNotFoundError:
+    print('The output file could not be created. The directory may be incorrect.\nExiting the program.')
+    exit(1)
+
+outputFile.write(
+        'UID, Preferred, Surname, Year, Academic, Count A, , UID, Preferred, Surname, Year, Merit, Count M\n')
+
 for record in listOutput:
-    print(record)
+    # Write UID Academic
+    outputFile.write(record[0])
+    outputFile.write(',')
+    # Write Preferred Name Academic
+    outputFile.write(record[1])
+    outputFile.write(',')
+    # Write Surname Academic
+    outputFile.write(record[2])
+    outputFile.write(',')
+    # Write Year Level Academic
+    outputFile.write(record[3])
+    outputFile.write(',')
+    # Write Subjects for Academic
+    for i in record[4]:
+        outputFile.write(i)
+        if i != record[4][-1]:
+            outputFile.write(';')
+    outputFile.write(',')
+    # Write Count A
+    record[5] = str(record[5])
+    outputFile.write(record[5])
+    outputFile.write(',')
+    # Write Blank Space
+    outputFile.write(record[6])
+    outputFile.write(',')
+    # Write UID Merit
+    outputFile.write(record[7])
+    outputFile.write(',')
+    # Write Preferred Name Merit
+    outputFile.write(record[8])
+    outputFile.write(',')
+    # Write Surname Merit
+    outputFile.write(record[9])
+    outputFile.write(',')
+    # Write Year Level Merit
+    outputFile.write(record[10])
+    outputFile.write(',')
+    # Write Subjects for Merit
+    for i in record[11]:
+        outputFile.write(i)
+        if i != record[11][-1]:
+            outputFile.write(';')
+    outputFile.write(',')
+    # Write Count M
+    record[12] = str(record[12])
+    outputFile.write(record[12])
+
+    outputFile.write('\n')
+outputFile.close()
+
+print("\nInput Files Processed. Output Saved at: '{0}'".format(fileName))
